0fef614fb2256997658fb92ffd799d1ac661f011
Guillaume Viguier-Just <guillaume@viguierjust.com>
2013-12-04 16:21:00 +0100

    Added dictionary

--- includes/dictionary.php
+++ includes/dictionary.php
@@ -1,43 +1,41 @@
 <?php
 
+class HRLatorDictionaryDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('dictionary.sqlite');
+    }
+}
+
 class HRLatorDictionary {
   
   protected $database = NULL;
 
   public function __construct() {
-    try {
-      //create or open the database
-      $this->database = new SQLiteDatabase('dictionary.sqlite', 0666, $error);
-    }
-    catch(Exception $e) 
-    {
-      die($error);
-    }
+    //create or open the database
+    $this->database = new HRLatorDictionaryDB();
     
     $query = "CREATE TABLE IF NOT EXISTS Dictionary ".
       "(Type TEXT, Initial TEXT, Replacement TEXT, PRIMARY KEY (Type, Initial, Replacement))";
       
-    if(!$this->database->queryExec($query, $error)) {
-      die($error);
-    }
+    $this->database->exec($query);
   }
   
   public function add($type, $initial, $replacement) {
     $query = 'INSERT INTO Dictionary (Type, Initial, Replacement)' .
       'VALUES ("'.$type.'", "'.$initial.'", "'.$replacement.'"); ';
-    if(!$this->database->queryExec($query, $error)) {
-      die($error);
-    }
+    $this->database->exec($query);
   }
   
   public function find($type, $initial) {
     $query = 'SELECT * FROM Dictionary WHERE Type = "'.$type.'" AND Initial = "'.$initial.'";';
-    if($result = $database->query($query, SQLITE_BOTH, $error)) {
+    if($result = $this->database->query($query)) {
       $row = $result->fetch();
       return $row['Replacement'];
     }
     else {
-      die($error);
+      return "";
     }
   }
   
