c94d43e20e48ce1c7a628d6a5407c7a44b4df350
Guillaume Viguier <guillaumev@hostingtest.humanitarianresponse.info>
2015-01-23 16:41:50 +0100

    Rewrote csv generation script in PHP

--- json2csv/json2csv.php
+++ json2csv/json2csv.php
@@ -9,18 +9,18 @@ if (!isset($_GET['api_path'])) {
 	<title>Convert HR.info API to CSV</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="json2csv.js"></script>
 </head>
 <body>
 	<h1>Convert HR.info API to CSV files</h1>
 	<p>This utility allows you to convert any data returned by the Humanitarianresponse.info API into CSV files. Just enter the path in the text input below and click on "Generate File".</p>
-	<div class="form-group">
-		<label for="api_path">API path</label>
-		<input type="text" class="form-control" id="api_path" placeholder="Enter path to API" aria-describedby="helpBlock">
-		<span id="helpBlock" class="help-block">Enter the path to the API that you want to convert to CSV. For example, if you want to convert global clusters, enter "/api/v1.0/global_clusters" (make sure you start with a /).</span>
-	</div>
-	<button class="btn btn-default">Generate File</button>
+        <form method="GET">
+	  <div class="form-group">
+	    <label for="api_path">API path</label>
+	    <input type="text" class="form-control" id="api_path" name="api_path" placeholder="Enter path to API" aria-describedby="helpBlock">
+	    <span id="helpBlock" class="help-block">Enter the path to the API that you want to convert to CSV. For example, if you want to convert global clusters, enter "/api/v1.0/global_clusters" (make sure you start with a /).</span>
+	  </div>
+	  <input type="submit" value="Generate File" />
+        </form>
 </body>
 </html>
 <?php
@@ -30,6 +30,52 @@ else {
   $path = 'http://www.humanitarianresponse.info' . $_GET['api_path'];
   $raw = file_get_contents($path);
   $data = json_decode($raw);
-  print_r($data);
+  $api_data = $data->data;
+  while (isset($data->next)) {
+    $raw = file_get_contents($data->next->href);
+    $data = json_decode($raw);
+    foreach ($data->data as $item) {
+      $api_data[] = $item;
+    }
+  }
+  // Set the headers we need for this to work
+  header('Content-Type: text/csv; charset=utf-8');
+  header('Content-Disposition: attachment; filename=data.csv');
+  $out = fopen('php://output', 'w');
+  $first_item = $api_data[0];
+  $headers = array_keys((array)$first_item);
+  fputcsv($out, $headers);
+  foreach ($api_data as $line) {
+    $array_line = (array)$line;
+    foreach ($array_line as &$item) {
+      $item = json2csv_get_item($item);
+    }
+    fputcsv($out, $array_line);
+  }
+  fclose($out);
 }
 
+function json2csv_get_item($item) {
+  if (gettype($item) == 'object') {
+    return $item->label;
+  }
+  elseif (gettype($item) == 'array') {
+    $tmp = '[';
+    $first = TRUE;
+    foreach ($item as $item_item) {
+      if ($first != TRUE) {
+        $tmp .= ',';
+      }
+      else {
+        $first = FALSE;
+      }
+      $tmp .= '"'.json2csv_get_item($item_item).'"';
+    }
+    $tmp .= ']';
+    return $tmp;
+  }
+  else {
+    return $item;
+  }
+}
+  
